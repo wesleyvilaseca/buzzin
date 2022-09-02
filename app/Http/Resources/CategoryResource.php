bacfa842ce945cd4d858fddce826ae63034be8ba
Wesley Sanches <wesley.sanches@vistasoft.com.br>
2022-09-02 16:47:15 -0300

    ajuste resource category

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -15,6 +15,7 @@ class CategoryResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->uuid,
             'name' => $this->name,
             'url' => $this->url,
             'icon' => $this->icon,
