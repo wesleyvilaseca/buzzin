4f2066aa546d9ebc5fe9e0ca57632f7b725b4eed
Wesley Vila Seca <wesley.vilaseca@hotmail.com>
2023-09-01 13:31:14 -0300

    Fix: chamada de libs js no tema site

--- resources/views/layouts/site/site.blade.php
+++ resources/views/layouts/site/site.blade.php
@@ -12,7 +12,6 @@
     <link rel="icon" href="{{ asset('images/favicon.png') }}">
     <link href="{{ asset('css/site.css') }}" rel="stylesheet">
 
-
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -23,6 +22,7 @@
     <!--jquery-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css'>
+
 </head>
 
 <body>
@@ -61,8 +61,11 @@
     </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="{{ asset('js/app.js') }}"></script>
+
+    @yield('js')
 </body>
 
 </html>
