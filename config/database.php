4bf7cf651bfecaef6e09a3aef3d263f81608ffd7
Wesley Vila Seca <wesley.vilaseca@hotmail.com>
2024-02-21 16:10:34 -0300

    ajuste redis config

--- config/database.php
+++ config/database.php
@@ -127,6 +127,7 @@ return [
         ],
 
         'default' => [
+            'prefix' => '',
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
@@ -141,7 +142,6 @@ return [
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
         ],
-
     ],
 
 ];
